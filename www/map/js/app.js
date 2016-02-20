b6833f4320cb18939f8afa9a529798a011e65647
mjaminbook <bmathers@macalester.edu>
2016-02-20 02:34:16 -0600

    For Prady

--- www/map/js/app.js
+++ www/map/js/app.js
@@ -1,29 +1,42 @@
 
+var inAddMode = false;
+
 $(document).ready(function(){
 	//For Adding events on the map
-	//if(inAddMode) do the following
-	$('#map').click(function(e){
-		var img = $('<img src="img/map_pin.png" />');
-		var mapWidth = $("#map_img").width();
-		var mapHeight = $("#map_img").height();
+		$('#map').click(function(e){
+			if(inAddMode){
+				var img = $('<img src="img/map_pin.png" />');
+				var mapWidth = $("#map_img").width();
+				var mapHeight = $("#map_img").height();
 
-		//Must keep the pixel ratio of original image constant
-		var mapPinWidth = mapWidth*(0.08);
-		var mapPinHeight = mapPinWidth;
+				//Must keep the pixel ratio of original image constant
+				var mapPinWidth = mapWidth*(0.08);
+				var mapPinHeight = mapPinWidth;
 
-		var offsetLeft = mapPinWidth/2;
-		var offsetTop = mapPinHeight;
+				var offsetLeft = mapPinWidth/2;
+				var offsetTop = mapPinHeight;
 
-		img.css({
-			position: 'absolute',
-			width: mapPinWidth,
-			height: mapPinHeight,
-			left: e.pageX-offsetLeft,
-			top: e.pageY-offsetTop,
-			cursor: 'pointer'
+				img.css({
+					position: 'absolute',
+					width: mapPinWidth,
+					height: mapPinHeight,
+					left: e.pageX-offsetLeft,
+					top: e.pageY-offsetTop,
+					cursor: 'pointer'
+				});
+				$("#map").append(img);
+			//Now make the app take you to the event creation page
+			}
 		});
-		$("#map").append(img);
-		//Now make the app take you to the event creation page
-	});
+
+
 	//end of addMode
 });
+
+$(document).ready(function(){
+			
+		$('#add_button').click(function(){
+			//inAddMode = true;
+			alert(inAddMode);
+		});
+})
