dba672545667d672741a23fd5a76f462cc9ad216
mjaminbook <bmathers@macalester.edu>
2016-02-20 01:01:20 -0600

    Can add pins to the map

---
+++ www/map/js/app.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+	//For Adding events on the map
+	//if(inAddMode) do the following
+	$('#map').click(function(e){
+		var img = $('<img src="img/map_pin.png" />');
+		var mapWidth = $("#map_img").width();
+		var mapHeight = $("#map_img").height();
+
+		//Must keep the pixel ratio of original image constant
+		var mapPinWidth = mapWidth*(0.08);
+		var mapPinHeight = mapPinWidth;
+
+		var offsetLeft = mapPinWidth/2;
+		var offsetTop = mapPinHeight;
+
+		img.css({
+			position: 'absolute',
+			width: mapPinWidth,
+			height: mapPinHeight,
+			left: e.pageX-offsetLeft,
+			top: e.pageY-offsetTop,
+			cursor: 'pointer'
+		});
+		$("#map").append(img);
+	});
+	//end of addMode
+});
